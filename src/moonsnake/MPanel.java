9f95307a9589accfb5b89891dd1117b29f634c9b
birdegg <313993678@qq.com>
2019-11-08 12:42:05 +0800

    show the static demo

--- src/moonsnake/MPanel.java
+++ src/moonsnake/MPanel.java
@@ -12,6 +12,12 @@ public class MPanel extends JPanel {
     String path = "src/resource/";
 
     ImageIcon title = new ImageIcon(path + "title.jpg");
+    ImageIcon body = new ImageIcon(path + "body.png");
+    ImageIcon up = new ImageIcon(path + "up.png");
+    ImageIcon down = new ImageIcon(path + "down.png");
+    ImageIcon left = new ImageIcon(path + "left.png");
+    ImageIcon right = new ImageIcon(path + "right.png");
+    ImageIcon food = new ImageIcon(path + "food.png");
 
 
 
@@ -25,5 +31,9 @@ public class MPanel extends JPanel {
         title.paintIcon(this, g, 25, 11);
 
         g.fillRect(25, 75, 850, 600);
+
+        right.paintIcon(this, g, 100, 100);
+        body.paintIcon(this, g, 75, 100);
+        body.paintIcon(this, g, 50, 100);
     }
 }
